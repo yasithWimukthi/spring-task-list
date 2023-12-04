4bfdc20234d7d9a12e63b8ad3d02d2ebedb5d9bc
yasithw <yasith.w@eight25media.com>
2023-12-04 19:29:18 +0530

    Initial commit

---
+++ src/main/java/com/alphacoder/tasklist/springtasklist/SpringTaskListApplication.java
@@ -0,0 +1,13 @@
+package com.alphacoder.tasklist.springtasklist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTaskListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTaskListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alphacoder/tasklist/springtasklist/SpringTaskListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alphacoder.tasklist.springtasklist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTaskListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
